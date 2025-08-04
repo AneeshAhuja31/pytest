0e84d43262af7424f0dbba131202e72b0583506f
Aneesh Ahuja <aneeshahuja31@gmail.com>
2025-08-05 00:24:23 +0530

    fixtures

--- source/shapes.py
+++ source/shapes.py
@@ -17,3 +17,21 @@ class Circle(Shape):
     
     def perimeter(self):
         return 2 * math.pi * self.radius
+    
+class Rectangle(Shape):
+
+    def __init__(self,length,breadth):
+        self.length = length
+        self.breadth = breadth
+    
+    def __eq__(self, other):
+        if not isinstance(other,Rectangle):
+            return False
+        
+        return self.breadth == other.breadth and self.length == other.length
+    
+    def area(self):
+        return self.length * self.breadth
+    
+    def perimeter(self):
+        return 2*(self.length+self.breadth)
